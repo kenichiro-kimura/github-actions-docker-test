c8eed346376ed4baaa48adc9c6b04249eae70aa6
k-kimura <k-kimura@alterbooth.com>
2022-09-02 15:50:10 +0900

    [add] add docker files

---
+++ docker/md2pdf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export LANG=ja_JP.UTF-8
+for file in $@;do
+    suf=`echo $file | sed 's/^.*\.\([^\.]*\)$/\1/'`
+    if [ $suf = 'md' ] ; then
+        outfile=`echo $file | sed 's/\//_/g' |sed 's/\.md$/\.pdf/i'`
+        cat $file | npx md-to-pdf --launch-options '{"args": ["--no-sandbox"]}' --md-file-encoding utf-8 > $outfile
+    fi
+done
+tar cvfz docs.tgz `find . -name '*.pdf'`
